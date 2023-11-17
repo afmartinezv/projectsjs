bb7f2d598ce997e7be7983c7e6ab0010bc06e861
afmartinezv <andresmartinezvaron@gmail.com>
2023-11-17 10:24:30 -0500

    Nuevos ejercicios

--- ejercicio.js
+++ ejercicio.js
@@ -1,4 +1,11 @@
-"use strict"
+"use strict";
+/* Preguntar nombre al usuario y saludarlo por su nombre. */
+/*
+const nombre = prompt('Por favor ingrese su nombre:');
+alert('Hola, ' + nombre);
+*/
+
+/*Preguntar nombre al usuario y saludarlo por su nombre. Si el usuario no escribe su nombre saludar: "Hola, desconocido"
 
 const nombre = prompt("Escriba su nombre");
 
@@ -9,4 +16,34 @@ if  (nombre){
 
     document.write("Hola desconocido");
 
+}*/
+
+/* 1. Leer nombre y apellido, saludar por nombre y apellido.
+const nombre = prompt("Por favor escriba su Nombre");
+const apellido = prompt("Por favor escriba su Apellido");
+
+    document.write("Hola," + " " + nombre + " " + apellido);
+*/
+/* 2. Pregunte al usuario si desea continuar, responder con hola o adiós.
+const continuar = prompt ("Desea continuar?");
+
+if(continuar === "si"){
+    alert("Hola")
+
+}else{
+    alert("Adios");
+
 }
+*/
+/*Leer nombre y edad, Mostrar si es o no mayor de edad.
+const nombre = prompt ("Por favor ingrese su nombre");
+const edad = prompt ("Por favor ingrese su edad");
+
+console.log(Number(edad));
+
+if(edad>=18){
+    document.write("Hola" + " " + nombre + "," +" " + "usted es mayor de edad");
+}else if (edad<18){
+    document.write("Hola" + " " + nombre + "," +" " + "ud es menor de edad");
+}
+*/
